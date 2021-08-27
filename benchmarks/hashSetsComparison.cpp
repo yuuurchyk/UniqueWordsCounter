54171c6e274af40dc6da2992f1befbed5ef9bf6b
Yurii Antentyk <yurii.antentyk@gmail.com>
2021-08-27 23:57:51 +0300

    Rewrite hashSetsComparison benchmark

--- benchmarks/hashSetsComparison.cpp
+++ benchmarks/hashSetsComparison.cpp
@@ -1,145 +1,108 @@
-#include <cstddef>
-#include <fstream>
-#include <limits>
+#include <functional>
+#include <memory>
 #include <random>
 #include <sstream>
 #include <stdexcept>
 #include <string>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
 #include <benchmark/benchmark.h>
 
+#include "UniqueWordsCounter/utils/getFile.h"
 #include "UniqueWordsCounter/utils/openAddressingSet.h"
 
 #include "utils/textFiles.h"
 
 namespace
 {
-std::vector<std::string> getWords()
+std::unique_ptr<const std::vector<std::string>> words;
+std::unique_ptr<const std::vector<size_t>>      hashes;
+
+class WordsFixture : public benchmark::Fixture
 {
-    const auto &filename = kEnglishWords;
+public:
+    std::vector<std::string> fixtureWords;
+    std::vector<size_t>      fixtureHashes;
 
-    auto file = std::ifstream{ filename };
-
-    if (!file.is_open())
-        throw std::runtime_error{ "Could not open file: " + filename };
-
-    auto words = std::vector<std::string>{};
-    auto word  = std::string{};
-
-    while (file >> word)
-        words.push_back(word);
-
-    auto l = words.size();
-    for (int j = 0; j < 200; ++j)
-        for (int i = 0; i < l; ++i)
-            words.push_back(words[i]);
-    std::random_shuffle(words.begin(), words.end());
-
-    return words;
-}
-
-std::vector<size_t> getHashes(const std::vector<std::string> &words)
-{
-    // auto hashes = std::vector<size_t>{};
-    // hashes.reserve(words.size());
-
-    // auto hashFunctor = std::hash<std::string>{};
-
-    // for (const auto &word : words)
-    //     hashes.push_back(hashFunctor(word));
-
-    // return hashes;
-
-    // ----------------------
-
-    auto hashes = std::vector<size_t>{};
-    hashes.reserve(words.size());
-
-    constexpr size_t kMod{ 618819619619660099ULL };
-    constexpr size_t kP{ 29 };
-
-    for (const auto &word : words)
+    void SetUp(const benchmark::State &state)
     {
-        size_t hash{};
-        for (const auto &character : word)
-            hash = (hash * kP + (character - 'a' + 1)) % kMod;
-        hashes.push_back(hash);
+        const auto wordsRequested = state.range(0);
+        if (wordsRequested < 0)
+        {
+            std::stringstream errorMessage;
+            errorMessage << "Benchmark can be run with at most " << words->size()
+                         << " words, requested " << wordsRequested;
+        }
+
+        fixtureWords.reserve(wordsRequested);
+        fixtureHashes.reserve(wordsRequested);
+
+        for (auto i = size_t{}; i < wordsRequested; ++i)
+        {
+            fixtureWords.push_back((*words)[i]);
+            fixtureHashes.push_back((*hashes)[i]);
+        }
+
+        for (const auto &item : fixtureHashes)
+            if (item == std::numeric_limits<size_t>::max())
+                throw std::runtime_error{
+                    "Hash value conflicts with OpenAddressingSet reserved value"
+                };
     }
-
-    return hashes;
-}
-
-void checkWordsNum(const std::vector<std::string> &words, int64_t wordsRequested)
-{
-    if (words.size() < wordsRequested)
+    void TearDown(const benchmark::State &state)
     {
-        std::stringstream errorMessage;
-        errorMessage << "Benchmark can be run with at most " << words.size()
-                     << " words, requested " << wordsRequested;
-
-        throw std::runtime_error{ errorMessage.str() };
+        fixtureWords.clear();
+        fixtureHashes.clear();
     }
-}
+};
 
-static void BM_unordered_set_of_strings(benchmark::State &state)
+BENCHMARK_DEFINE_F(WordsFixture, BM_unordered_set_of_strings)(benchmark::State &state)
 {
-    const auto &words = getWords();
-    checkWordsNum(words, state.range(0));
-
     auto size = size_t{};
 
     for (auto _ : state)
     {
         auto uniqueWords = std::unordered_set<std::string>{};
-        for (auto i = size_t{}; i < state.range(0); ++i)
-            uniqueWords.insert(words[i]);
+
+        for (auto &item : fixtureWords)
+            uniqueWords.emplace(item.data(), item.size());
+
         benchmark::DoNotOptimize(size = uniqueWords.size());
     }
 
     state.counters["uniqueWords"] = size;
 }
 
-static void BM_unordered_set_of_hashes(benchmark::State &state)
+BENCHMARK_DEFINE_F(WordsFixture, BM_unordered_set_of_hashes)(benchmark::State &state)
 {
-    const auto &words = getWords();
-    checkWordsNum(words, state.range(0));
-
-    const auto &hashes = getHashes(words);
-
     auto size = size_t{};
 
     for (auto _ : state)
     {
         auto uniqueHashes = std::unordered_set<size_t>{};
-        for (auto i = size_t{}; i < state.range(0); ++i)
-            uniqueHashes.insert(hashes[i]);
+
+        for (const auto &item : fixtureHashes)
+            uniqueHashes.insert(item);
+
         benchmark::DoNotOptimize(size = uniqueHashes.size());
     }
 
     state.counters["uniqueHashes"] = size;
 }
 
-static void BM_open_address_set_of_hashes(benchmark::State &state)
+BENCHMARK_DEFINE_F(WordsFixture, BM_open_address_set_of_hashes)(benchmark::State &state)
 {
-    const auto &words = getWords();
-    checkWordsNum(words, state.range(0));
-
-    const auto &hashes = getHashes(words);
-    for (const auto &item : hashes)
-        if (item == std::numeric_limits<size_t>::max())
-            throw std::runtime_error{
-                "Hash value conflicts with OpenAddressingSet reserved value"
-            };
-
     auto size = size_t{};
 
     for (auto _ : state)
     {
         auto uniqueHashes = OpenAddressingSet{};
-        for (auto i = size_t{}; i < state.range(0); ++i)
-            uniqueHashes.insert(hashes[i]);
+
+        for (const auto &item : fixtureHashes)
+            uniqueHashes.insert(item);
+
         benchmark::DoNotOptimize(size = uniqueHashes.size());
     }
 
@@ -148,17 +111,55 @@ static void BM_open_address_set_of_hashes(benchmark::State &state)
 
 }    // namespace
 
-// BENCHMARK(BM_unordered_set_of_strings)
-//     ->RangeMultiplier(1 << 2)
-//     ->Range(1 << 12, 1 << 24)
-//     ->Unit(benchmark::kMillisecond);
-BENCHMARK(BM_unordered_set_of_hashes)
+BENCHMARK_REGISTER_F(WordsFixture, BM_unordered_set_of_strings)
     ->RangeMultiplier(1 << 3)
-    ->Range(1 << 12, 1 << 25)
+    ->Range(1 << 12, 1 << 24)
     ->Unit(benchmark::kMillisecond);
-BENCHMARK(BM_open_address_set_of_hashes)
+BENCHMARK_REGISTER_F(WordsFixture, BM_unordered_set_of_hashes)
     ->RangeMultiplier(1 << 3)
-    ->Range(1 << 12, 1 << 25)
+    ->Range(1 << 12, 1 << 24)
+    ->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(WordsFixture, BM_open_address_set_of_hashes)
+    ->RangeMultiplier(1 << 3)
+    ->Range(1 << 12, 1 << 24)
     ->Unit(benchmark::kMillisecond);
 
-BENCHMARK_MAIN();
+int main(int argc, char **argv)
+{
+    {
+        auto words = new std::vector<std::string>{};
+
+        for (const auto filename :
+             { kSyntheticShortWords100MB, kSyntheticLongWords100MB })
+        {
+            auto file = getFile(filename.c_str());
+            auto word = std::string{};
+
+            while (file >> word)
+                words->push_back(word);
+        }
+
+        std::shuffle(words->begin(), words->end(), std::mt19937{ 47 });
+
+        ::words.reset(words);
+    }
+    {
+        auto hashes = new std::vector<size_t>{};
+        hashes->reserve(words->size());
+
+        auto hashFunctor = std::hash<std::string>{};
+
+        for (const auto &word : *words)
+            hashes->push_back(hashFunctor(word));
+
+        ::hashes.reset(hashes);
+    }
+
+    // expansion of BENCHMARK_MAIN() macro
+    ::benchmark::Initialize(&argc, argv);
+    if (::benchmark::ReportUnrecognizedArguments(argc, argv))
+        return 1;
+    ::benchmark::RunSpecifiedBenchmarks();
+    ::benchmark::Shutdown();
+    return 0;
+}
