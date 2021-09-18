6dd21ac8b0203e85e517b74f4f173934b402d21d
Yurii Antentyk <yurii.antentyk@gmail.com>
2021-09-18 03:38:59 +0300

    Fix todo in hash test

--- tests/hash.cpp
+++ tests/hash.cpp
@@ -1,18 +1,11 @@
-#include <algorithm>
-#include <concepts>
 #include <cstddef>
 #include <iostream>
-#include <iterator>
-#include <limits>
 #include <memory>
-#include <sstream>
 #include <string>
 #include <tuple>
 #include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
-#include <utility>
-#include <vector>
 
 #include "gtest/gtest.h"
 
@@ -21,29 +14,26 @@
 
 namespace
 {
-// TODO: consider specifying template requirements in template function
+// clang-format off
 template <typename F>
-concept HashFunction = requires(F)
-{
-    std::invocable<F, const char *, size_t>;
-    std::is_integral_v<std::invoke_result_t<F, const char *, size_t>>;
-};
-template <HashFunction F>
-using hash_type = std::invoke_result_t<F, const char *, size_t>;
-
-template <HashFunction F>
+requires(
+    std::invocable<F, const char *, size_t> &&
+    std::is_integral_v<std::invoke_result_t<F, const char *, size_t>>
+)
 auto getNumberOfAmbiguousHashValues(F hashFunction) -> std::tuple<size_t, std::string>
+// clang-format on
 {
     using namespace UniqueWordsCounter::Utils::TextFiles;
+    using hash_type = std::invoke_result_t<F, const char *, size_t>;
 
-    auto uniqueHashes    = std::unordered_map<hash_type<F>, std::string>{};
-    auto ambiguousHashes = std::unordered_set<hash_type<F>>{};
+    auto uniqueHashes    = std::unordered_map<hash_type, std::string>{};
+    auto ambiguousHashes = std::unordered_set<hash_type>{};
 
     for (const auto &word : WordsGenerator{ kAllFiles })
     {
         const auto &hash = hashFunction(word.data(), word.size());
 
-        if (ambiguousHashes.find(hash) != ambiguousHashes.end())
+        if (ambiguousHashes.contains(hash))
             continue;
 
         auto [it, success] = uniqueHashes.insert({ hash, word });
@@ -57,10 +47,10 @@ auto getNumberOfAmbiguousHashValues(F hashFunction) -> std::tuple<size_t, std::s
 
     const auto numberOfAmbiguousHashValues = ambiguousHashes.size();
 
-    auto diagnosticMessage = std::stringstream{};
-    diagnosticMessage << "Found " << numberOfAmbiguousHashValues << " hash values";
-
-    return std::make_tuple(numberOfAmbiguousHashValues, diagnosticMessage.str());
+    using namespace std::string_literals;
+    return std::make_tuple(numberOfAmbiguousHashValues,
+                           "Found "s + std::to_string(numberOfAmbiguousHashValues) +
+                               " ambiguous hash values."s);
 }
 
 }    // namespace
