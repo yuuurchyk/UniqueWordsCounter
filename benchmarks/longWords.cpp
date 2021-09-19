6df752343b519dcb44f8cf7522a5986b6179b916
Yurii Antentyk <yurii.antentyk@gmail.com>
2021-09-19 13:54:57 +0300

    Add GCC benchmarks results

--- benchmarks/longWords.cpp
+++ benchmarks/longWords.cpp
@@ -2,34 +2,34 @@
 
 #include <benchmark/benchmark.h>
 
-REGISTER_BENCHMARK(baseline, Long, 10, 5);
-REGISTER_BENCHMARK(customScanning, Long, 10, 5);
-REGISTER_BENCHMARK(optimizedBaseline, Long, 10, 5);
-REGISTER_BENCHMARK1(producerConsumer, Long, 10, 5, jobs, 3);
-REGISTER_BENCHMARK1(producerConsumer, Long, 10, 5, jobs, 4);
-REGISTER_BENCHMARK1(producerConsumer, Long, 10, 5, jobs, 6);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 10, 5, jobs, 3);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 10, 5, jobs, 4);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 10, 5, jobs, 6);
-REGISTER_BENCHMARK1(optimizedProducerConsumer, Long, 10, 5, jobs, 3);
+REGISTER_BENCHMARK(baseline, Long, 10, 10);
+REGISTER_BENCHMARK(customScanning, Long, 10, 10);
+REGISTER_BENCHMARK(optimizedBaseline, Long, 10, 10);
+REGISTER_BENCHMARK1(producerConsumer, Long, 10, 10, jobs, 3);
+REGISTER_BENCHMARK1(producerConsumer, Long, 10, 10, jobs, 4);
+REGISTER_BENCHMARK1(producerConsumer, Long, 10, 10, jobs, 6);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 10, 10, jobs, 3);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 10, 10, jobs, 4);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 10, 10, jobs, 6);
+REGISTER_BENCHMARK1(optimizedProducerConsumer, Long, 10, 10, jobs, 3);
 
-REGISTER_BENCHMARK(baseline, Long, 100, 5);
-REGISTER_BENCHMARK(customScanning, Long, 100, 5);
-REGISTER_BENCHMARK(optimizedBaseline, Long, 100, 5);
-REGISTER_BENCHMARK1(producerConsumer, Long, 100, 5, jobs, 3);
-REGISTER_BENCHMARK1(producerConsumer, Long, 100, 5, jobs, 4);
-REGISTER_BENCHMARK1(producerConsumer, Long, 100, 5, jobs, 6);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 100, 5, jobs, 3);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 100, 5, jobs, 4);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 100, 5, jobs, 6);
-REGISTER_BENCHMARK1(optimizedProducerConsumer, Long, 100, 5, jobs, 3);
+REGISTER_BENCHMARK(baseline, Long, 100, 10);
+REGISTER_BENCHMARK(customScanning, Long, 100, 10);
+REGISTER_BENCHMARK(optimizedBaseline, Long, 100, 10);
+REGISTER_BENCHMARK1(producerConsumer, Long, 100, 10, jobs, 3);
+REGISTER_BENCHMARK1(producerConsumer, Long, 100, 10, jobs, 4);
+REGISTER_BENCHMARK1(producerConsumer, Long, 100, 10, jobs, 6);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 100, 10, jobs, 3);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 100, 10, jobs, 4);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 100, 10, jobs, 6);
+REGISTER_BENCHMARK1(optimizedProducerConsumer, Long, 100, 10, jobs, 3);
 
-REGISTER_BENCHMARK(baseline, Long, 1000, 1);
-REGISTER_BENCHMARK(customScanning, Long, 1000, 1);
-REGISTER_BENCHMARK(optimizedBaseline, Long, 1000, 1);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 1000, 1, jobs, 3);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 1000, 1, jobs, 4);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 1000, 1, jobs, 6);
-REGISTER_BENCHMARK1(optimizedProducerConsumer, Long, 1000, 1, jobs, 3);
+REGISTER_BENCHMARK(baseline, Long, 1000, 5);
+REGISTER_BENCHMARK(customScanning, Long, 1000, 5);
+REGISTER_BENCHMARK(optimizedBaseline, Long, 1000, 5);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 1000, 5, jobs, 3);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 1000, 5, jobs, 4);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Long, 1000, 5, jobs, 6);
+REGISTER_BENCHMARK1(optimizedProducerConsumer, Long, 1000, 5, jobs, 3);
 
 BENCHMARK_MAIN();
--- benchmarks/shortWords.cpp
+++ benchmarks/shortWords.cpp
@@ -2,34 +2,34 @@
 
 #include <benchmark/benchmark.h>
 
-REGISTER_BENCHMARK(baseline, Short, 10, 5);
-REGISTER_BENCHMARK(customScanning, Short, 10, 5);
-REGISTER_BENCHMARK(optimizedBaseline, Short, 10, 5);
-REGISTER_BENCHMARK1(producerConsumer, Short, 10, 5, jobs, 3);
-REGISTER_BENCHMARK1(producerConsumer, Short, 10, 5, jobs, 4);
-REGISTER_BENCHMARK1(producerConsumer, Short, 10, 5, jobs, 6);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 10, 5, jobs, 3);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 10, 5, jobs, 4);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 10, 5, jobs, 6);
-REGISTER_BENCHMARK1(optimizedProducerConsumer, Short, 10, 5, jobs, 3);
+REGISTER_BENCHMARK(baseline, Short, 10, 10);
+REGISTER_BENCHMARK(customScanning, Short, 10, 10);
+REGISTER_BENCHMARK(optimizedBaseline, Short, 10, 10);
+REGISTER_BENCHMARK1(producerConsumer, Short, 10, 10, jobs, 3);
+REGISTER_BENCHMARK1(producerConsumer, Short, 10, 10, jobs, 4);
+REGISTER_BENCHMARK1(producerConsumer, Short, 10, 10, jobs, 6);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 10, 10, jobs, 3);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 10, 10, jobs, 4);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 10, 10, jobs, 6);
+REGISTER_BENCHMARK1(optimizedProducerConsumer, Short, 10, 10, jobs, 3);
 
-REGISTER_BENCHMARK(baseline, Short, 100, 5);
-REGISTER_BENCHMARK(customScanning, Short, 100, 5);
-REGISTER_BENCHMARK(optimizedBaseline, Short, 100, 5);
-REGISTER_BENCHMARK1(producerConsumer, Short, 100, 5, jobs, 3);
-REGISTER_BENCHMARK1(producerConsumer, Short, 100, 5, jobs, 4);
-REGISTER_BENCHMARK1(producerConsumer, Short, 100, 5, jobs, 6);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 100, 5, jobs, 3);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 100, 5, jobs, 4);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 100, 5, jobs, 6);
-REGISTER_BENCHMARK1(optimizedProducerConsumer, Short, 100, 5, jobs, 3);
+REGISTER_BENCHMARK(baseline, Short, 100, 10);
+REGISTER_BENCHMARK(customScanning, Short, 100, 10);
+REGISTER_BENCHMARK(optimizedBaseline, Short, 100, 10);
+REGISTER_BENCHMARK1(producerConsumer, Short, 100, 10, jobs, 3);
+REGISTER_BENCHMARK1(producerConsumer, Short, 100, 10, jobs, 4);
+REGISTER_BENCHMARK1(producerConsumer, Short, 100, 10, jobs, 6);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 100, 10, jobs, 3);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 100, 10, jobs, 4);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 100, 10, jobs, 6);
+REGISTER_BENCHMARK1(optimizedProducerConsumer, Short, 100, 10, jobs, 3);
 
-REGISTER_BENCHMARK(baseline, Short, 1000, 1);
-REGISTER_BENCHMARK(customScanning, Short, 1000, 1);
-REGISTER_BENCHMARK(optimizedBaseline, Short, 1000, 1);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 1000, 1, jobs, 3);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 1000, 1, jobs, 4);
-REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 1000, 1, jobs, 6);
-REGISTER_BENCHMARK1(optimizedProducerConsumer, Short, 1000, 1, jobs, 3);
+REGISTER_BENCHMARK(baseline, Short, 1000, 5);
+REGISTER_BENCHMARK(customScanning, Short, 1000, 5);
+REGISTER_BENCHMARK(optimizedBaseline, Short, 1000, 5);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 1000, 5, jobs, 3);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 1000, 5, jobs, 4);
+REGISTER_BENCHMARK1(concurrentSetProducerConsumer, Short, 1000, 5, jobs, 6);
+REGISTER_BENCHMARK1(optimizedProducerConsumer, Short, 1000, 5, jobs, 3);
 
 BENCHMARK_MAIN();
